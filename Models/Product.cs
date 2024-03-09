1aa67072463dd4cd64e9d8d0144fa375f63c124c
Isaev-El <isaev.elkhan2003@gmail.com>
2024-03-10 01:47:18 +0500

    add Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+namespace LuiMVC.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
